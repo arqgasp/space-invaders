ee914e92dd38135d6f006b128d5ceb80a73f9182
arqgasp <gabrielsilva.arq@gmail.com>
2022-06-11 07:18:22 -0300

    finished all part of player

--- index.js
+++ index.js
@@ -6,39 +6,139 @@ canvas.height = innerHeight
 
 class Player {
     constructor() {
-        this.position = {
-            x: 200,
-            y: 200
-        }
 
         this.velocity = {
             x: 0,
             y: 0
         }
 
+        this.rotation = 0
+
         const image = new Image()
         image.src = './img/spaceship.png'
-
-        this.image = image
-        this.width = 100
-        this.height = 100
+        image.onload = () => {
+            const scale = 0.15
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 20
+            }
+        }
     }
 
     draw() {
     //    c.fillStyle = 'red'
     //    c.fillRect(this.position.x, this.position.y, this.width, this.height) 
-       c.drawImage(this.image, this.position.x, this.position.y)
+
+        c.save()
+        c.translate(
+            player.position.x + player.width /2, 
+            player.position.y + player.height /2
+            )
+
+        c.rotate(this.rotation)
+
+        c.translate(
+        -player.position.x - player.width /2, 
+        -player.position.y - player.height /2
+        )
+
+       c.drawImage(
+        this.image, 
+        this.position.x, 
+        this.position.y, 
+        this.width, 
+        this.height
+        )
+        c.restore()
+    }
+    update(){
+        if (this.image) {
+       this.draw() 
+       this.position.x += this.velocity.x
+        }
     }
 }
 
+class Projectile {
+constructor({ position, velocity }){
+    this.position = position
+    this.velocity = this.velocity
+
+    this.radius = 3
+}
+
+}
+
 const player = new Player()
-player.draw()
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    player.draw()
+    player.update()
+
+    if (keys.a.pressed && player.position.x >= 0){
+        player.velocity.x = -7
+        player.rotation = -0.15
+    } else if (
+        keys.d.pressed && 
+        player.position.x + player.width <= canvas.width
+        ) {
+        player.velocity.x = 7
+        player.rotation = 0.15
+    } else {
+        player.velocity.x = 0
+        player.rotation = 0
+    }
 }
 
+
 animate()
+
+addEventListener('keydown', ({ key }) =>{
+    switch (key) {
+        case 'a':
+            console.log( 'left')
+
+            keys.a.pressed = true
+            break
+        case 'd':
+            console.log( 'right')
+            keys.d.pressed = true
+            break
+        case ' ':
+            console.log( 'space')
+            break
+    }
+})
+
+addEventListener('keyup', ({ key }) =>{
+    switch (key) {
+        case 'a':
+            console.log( 'left')
+            keys.a.pressed = false
+            break
+        case 'd':
+            console.log( 'right')
+            keys.d.pressed = false
+            break
+        case ' ':
+            console.log( 'space')
+            break
+    }
+})
